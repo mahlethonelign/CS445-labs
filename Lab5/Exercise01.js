97700ff3ccf8fa2917b1461f5a8b74e151d50156
Mahlet Honelign <mhonelign@miu.edu>
2021-11-02 23:10:57 -0500

    Lab5

---
+++ Lab5/Exercise01.js
@@ -0,0 +1,20 @@
+let isPrime = function (num) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+                if (num % i === 0) {
+                    reject({prime: false});
+                } else {
+                    resolve({prime: true});
+
+                }
+            }
+
+        }, 500)
+    })
+}
+console.log('start');
+isPrime(7)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+console.log('end');
---
+++ Lab5/Exercise02.js
@@ -0,0 +1,15 @@
+Array.prototype.removeDuplicatesAsync = function () {
+    let arr = this;
+    let newArr = [];
+    return new Promise(function (resolve, reject) {
+        for (num of arr) {
+            if (!newArr.includes(num)) {
+                newArr.push(num);
+            }
+        }
+        resolve(newArr)
+    }).then(console.log);
+}
+console.log(`start`);
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync();
+console.log(`end`);
