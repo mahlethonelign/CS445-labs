0fa8f45377bf27693723fafc5f22b54978c30589
Mahlet Honelign <92888810+amayah2021@users.noreply.github.com>
2021-11-04 12:40:33 -0500

    Update Exercise02.js

--- Lab6/Exercise02.js
+++ Lab6/Exercise02.js
@@ -1,19 +1,17 @@
-Array.prototype.removeDuplicates = function () {
-    let arr = this;
+
+Array.prototype.removeDuplicatesAsync = async function () {
+    console.log(await removeDuplicate(this));
+
+}
+function removeDuplicate(arr){
     let newArr = [];
     for (num of arr) {
         if (!newArr.includes(num)) {
             newArr.push(num);
         }
     }
-    return new Promise(function (resolve, reject) {
-        resolve(newArr)
-    });
-}
-
-Array.prototype.removeDuplicatesAsync = async function () {
-    console.log(await this.removeDuplicates());
-
+    return newArr;
+    
 }
 console.log(`start`);
 [4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync();
