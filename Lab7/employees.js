daf9ec041a9ff645742e2a09f4529ef8bfbc6baa
Mahlet Honelign <mhonelign@miu.edu>
2021-11-08 20:32:22 -0600

    Lab7

---
+++ Lab7/employees.js
@@ -0,0 +1,27 @@
+
+
+
+window.onload = function () {
+    loadEmployees();
+    document.getElementById("refresh").onclick = loadEmployees;
+}
+async function loadEmployees() {
+    document.getElementById("list").innerHTML = "";
+    let result = await fetch("https://randomuser.me/api/?results=5");
+    let employees = (await result.json()).results;
+    for (let i = 0; i < employees.length; i++) {
+        let emp = employees[i];
+        let info = document.createElement('div');
+        info.className = "row";
+        info.innerHTML = `<div class="col-4">
+                            <img src=${emp.picture.medium}>
+                        </div>
+                        <div class="col-8 text-end">
+                            <p><b>${emp.name.first + " " + emp.name.last}</b><br>
+                                ${emp.phone}<br>
+                                ${emp.email}
+                            </p>
+                        </div> <hr> `
+        document.getElementById("list").appendChild(info);
+    }
+}
